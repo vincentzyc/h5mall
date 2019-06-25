22d88da04434ab9819a43c6221e4a8c809276184
Vincentyun <vincentzyc@qq.com>
2019-06-25 18:54:17 +0800

    recommend

--- src/views/home/recommend.vue
+++ src/views/home/recommend.vue
@@ -10,10 +10,22 @@
     <div class="list">
       <div v-for="(item, index) in tabs" :label="item.label" :key="item.label+index" class="flex item">
         <img :src="require(`@/assets/img/${item.img}`)" alt class="item-img">
-        <div class="item-content mg-l10">
-          <h5 class="fs12 lh30">{{item.label}}</h5>
-          <h5 class="fs12 lh30">{{item.label}}</h5>
-          <h5 class="fs12 lh30">{{item.label}}</h5>
+        <div class="item-content mg-l10 flex-auto">
+          <h4 class="bold txtover">{{item.title}}</h4>
+          <p class="fs12 lh24 c999 txtover">{{item.introduce}}</p>
+          <div class="tag">
+            <span>限时特价</span>
+            <span>新品上市</span>
+            <span>包邮</span>
+          </div>
+          <div class="price lh30 ctheme">￥33</div>
+          <div class="flex fs12">
+            <div class="adress flex-auto">
+              <i class="cubeic-location"></i>
+              <span class="c666">广东省广州市</span>
+            </div>
+            <span class="sale">已售：3</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,13 +37,16 @@ export default {
   data() {
     return {
       tabs: [{
-        label: '京东自营',
+        title: '京东自营字母哥力压哈登当选常规赛MVP泪洒颁奖礼',
+        introduce: '111 1111 111 字母哥力压哈登当选常规赛MVP泪洒颁奖礼',
         img: 'store1.png'
       }, {
-        label: '竹海李氏山货',
+        title: '竹海李氏山货',
+        introduce: '222 222 22',
         img: 'store2.png'
       }, {
-        label: '周氏野货',
+        title: '周氏野货',
+        introduce: '333 33 333 3',
         img: 'store3.png'
       }]
     }
@@ -54,11 +69,24 @@ export default {
     .item {
       padding: 10px;
       border-bottom: 1px solid #eee;
+    }
 
-      .item-img {
-        width: 100px;
-        height: 100px;
-      }
+    .item-img {
+      width: 100px;
+      height: 100px;
+    }
+
+    .item-content {
+      overflow: hidden;
+    }
+
+    .adress i {
+      color: #55b0e0;
+      margin-right:2px;
+    }
+
+    .sale {
+      color: #ff8b1c;
     }
   }
 }
