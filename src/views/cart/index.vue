8c905b667b8ade82087b8e64b61769f3853f938c
Vincentyun <vincentzyc@qq.com>
2019-07-04 18:11:32 +0800

    cart

--- src/views/cart/index.vue
+++ src/views/cart/index.vue
@@ -2,32 +2,34 @@
   <base-page>
     <header class="header-bg">
       <div class="flex fs14 cfff">
-        <span class="col-2"></span>
+        <div class="col-2"></div>
         <div class="flex-auto">购物车</div>
-        <span class="col-2" @click="showDelete=!showDelete">{{showDelete?'完成':'编辑'}}</span>
+        <div class="col-2">
+          <span v-if="stores.length>0" @click="showDelete=!showDelete">{{showDelete?'完成':'编辑'}}</span>
+        </div>
       </div>
     </header>
-    <div class="content-wrap" v-if="items.length>0">
+    <div class="content-wrap" v-if="stores.length>0">
       <div class="content">
         <cube-scroll ref="scroll">
           <div class="scroll-wrapper">
             <!-- 店铺 -->
-            <div class="mg-t10" v-for="store in 3" :key="store">
+            <div class="mg-t10" v-for="(store,index) in stores" :key="store.storeId">
               <div class="flex align-middle pd10 c666 bgfff">
-                <cube-checkbox v-model="check" class="store-check"></cube-checkbox>
+                <cube-checkbox v-model="store.allCheck" class="store-check" @input="storeCheck(index)"></cube-checkbox>
                 <span class="store"></span>
-                <span class="mg-l10 mg-r10">京东自营{{store}}</span>
+                <span class="mg-l10 mg-r10">{{store.storeName}}</span>
                 <i class="fs16 cubeic-arrow"></i>
               </div>
               <!-- 店铺 商品 -->
-              <div class="product-item" v-for="item in store" :key="item">
+              <div class="product-item" v-for="item in store.products" :key="item.id">
                 <div class="pd10">
-                  <cube-checkbox v-model="checkitem">
+                  <cube-checkbox v-model="item.check" @input="itemCheck(index)">
                     <div class="flex product-info">
-                      <img src="@/assets/img/store3.png" alt="店铺logo" />
+                      <img :src="require(`@/assets/img/${item.img}`)" alt="店铺logo" />
                       <div class="pd-l10">
-                        <h4 class="textover2 mg-t5">梅西连续9次大赛无缘冠军 “心疼梅西”登上热搜梅西连续9次大赛无缘冠军 “心疼梅西”登上热搜</h4>
-                        <p class="ctheme fs16 mg-t5">￥9{{item}}</p>
+                        <h4 class="textover2 mg-t5">{{item.title}}</h4>
+                        <p class="ctheme fs16 mg-t5">￥{{item.price}}</p>
                       </div>
                     </div>
                   </cube-checkbox>
@@ -36,9 +38,9 @@
                   <div class="flex align-middle mg-l20">
                     <span class="mg-l10 c666 fs12">购买数量</span>
                     <div class="flex align-middle flex-auto justify-end">
-                      <span class="icon cut"></span>
-                      <span class="mg5">1{{item}}</span>
-                      <span class="icon add"></span>
+                      <span class="icon cut" @click="item.number = item.number>1?item.number-1:1"></span>
+                      <span class="mg5">{{item.number}}</span>
+                      <span class="icon add" @click="item.number = item.number+1"></span>
                     </div>
                   </div>
                 </div>
@@ -52,12 +54,15 @@
       <footer class="footer-wrap lh50">
         <div class="flex">
           <div class="flex pd-l10 c666">
-            <cube-checkbox v-model="allSelect">全选</cube-checkbox>
+            <cube-checkbox v-model="allSelect" @input="handleAllSelect">全选</cube-checkbox>
           </div>
           <div class="flex-auto">
-            <div class="text-right pd-r10 c666" v-show="!showDelete">总计：￥99.99</div>
+            <div class="text-right pd-r10 c666" v-show="!showDelete">
+              总计：
+              <span class="fs16">￥{{totalPrice}}</span>
+            </div>
           </div>
-          <div class="col-3 pay cfff" :class="showDelete?'bgorange':'bgtheme'">{{showDelete?'删除':'结算'}}</div>
+          <div class="col-3 pay cfff" :class="showDelete?'bgorange':'bgtheme'" @click="showDelete?delect():pay()">{{showDelete?'删除':'结算'}}</div>
         </div>
       </footer>
     </div>
@@ -76,6 +81,7 @@
 
 <script>
 import vRecommend from "@/components/recommend.vue";
+import { debuglog } from 'util';
 export default {
   name: "cart",
   components: {
@@ -87,9 +93,38 @@ export default {
       checkitem: false,
       showDelete: false,
       allSelect: false,
-      items: [
-
-      ],
+      stores: [{
+        storeId: 1,
+        storeName: '京东自营',
+        allCheck: false,
+        products: [{
+          check: false,
+          number: 1,
+          title: '字母哥力压哈登当选常规赛MVP泪洒颁奖礼',
+          img: 'store1.png',
+          price: 99,
+          id: 1
+        }, {
+          check: false,
+          number: 1,
+          title: 'NBA正式讨论减少常规赛场次 考虑增设季中冠军杯',
+          img: 'store2.png',
+          price: 199,
+          id: 2
+        }],
+      }, {
+        storeId: 2,
+        storeName: '周氏云商城',
+        allCheck: false,
+        products: [{
+          check: false,
+          number: 1,
+          title: '杜兰特成为完全自由球员加盟篮网',
+          img: 'store3.png',
+          price: 899,
+          id: 3
+        }],
+      }],
       recommendList: [{
         title: '常规赛MVP',
         introduce: '字母哥力压哈登当选常规赛MVP泪洒颁奖礼',
@@ -111,18 +146,44 @@ export default {
       }]
     }
   },
-  methods: {
-    test() {
-      console.log(111111);
+  computed: {
+    totalPrice() {
+      let totalPrice = 0, sum = 0;
+      this.stores.forEach(v => {
+        sum = v.products.reduce((all, c) => c.check ? all + c.number * c.price : all, 0)
+        totalPrice += sum;
+      })
+      return totalPrice
     }
   },
-  mounted() {
-    // this.$nextTick(function () {
-    //   setTimeout(() => {
-    //     this.$refs.slide.refresh();
-    //     this.$refs.scroll.refresh();
-    //   }, 0);
-    // });
+  methods: {
+    itemCheck(index) {
+      this.stores[index].allCheck = this.stores[index].products.every(v => v.check === true);
+      this.allSelect = this.stores.every(v => v.allCheck === true);
+    },
+    storeCheck(index) {
+      this.stores[index].products.forEach(v => v.check = this.stores[index].allCheck);
+      this.allSelect = this.stores.every(v => v.allCheck === true);
+    },
+    handleAllSelect(isSelect) {
+      this.stores.forEach(s => {
+        s.allCheck = isSelect;
+        s.products.forEach(v => v.check = isSelect)
+      })
+    },
+    delect() {
+      // this.stores.forEach((s, si,sarr) => {
+      //   console.log(si,sarr);
+      //   if (s.allCheck) {
+      //     sarr.splice(si, 1)
+      //   } else {
+      //     s.products.forEach((v, vi,varr) => {
+      //       if (v.check) varr.products.splice(vi, 1)
+      //     })
+      //   }
+      // })
+    },
+    pay() { }
   }
 };
 </script>
