1a9463814ddee9e1b6265df19c4a360e2efe96f3
vincentzyc <vincentzyc@qq.com>
2019-07-14 23:38:17 +0800

    side-nav

---
+++ src/components/side-nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+    <ul class="flex-none col-4">
+      <li v-for="item in data" :key="item.label" @click="selectNav=item" class="lh30 border-bddd">{{item.label}}</li>
+    </ul>
+    <ul class="flex-auto">
+      <li v-for="item in selectNav.items" :key="item" class="lh30 border-bddd">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectNav: null
+    }
+  }
+}
+</script>
--- src/views/products/search/citys.vue
+++ src/views/products/search/citys.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <div class="lh30">城市</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">2222222222</div>
-    <div class="lh30">城市</div>
+    <SideNav :data="address" />
   </div>
 </template>
+<script>
+import SideNav from '@/components/side-nav';
+const address = [
+  {
+    "label": "北京",
+    "items": ['北京']
+  },
+  {
+    "label": "广东省",
+    "items": ['广州', '深圳', '珠海', '汕头', '韶关', '佛山', '江门', '湛江', '茂名', '肇庆', '惠州', '梅州', '汕尾', '河源', '阳江', '清远', '东莞', '中山', '潮州', '揭阳', '云浮']
+  },
+  {
+    "label": "上海",
+    "items": ['上海']
+  },
+  {
+    "label": "天津",
+    "items": ['天津']
+  },
+  {
+    "label": "重庆",
+    "items": ['重庆']
+  }
+]
+export default {
+  components: {
+    SideNav
+  },
+  data() {
+    return {
+      address: address
+    }
+  }
+}
+</script>
