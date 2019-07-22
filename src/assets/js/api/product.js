5bebbdf8f3d4e6db706cfdafb6996e52588bc4b9
Vincentyun <vincentzyc@qq.com>
2019-07-22 16:35:48 +0800

    home-tabs

--- src/assets/js/api/product.js
+++ src/assets/js/api/product.js
@@ -1,6 +1,10 @@
 export default [{
-  name: "sysRecommend", //方法名
-  url: "/appProduct/sysRecommend" //接口路径
+  name: "productType", 
+  url: "/appProduct/productType", //商品分类(一级分类)
+  encrypt: false
+},{
+  name: "sysRecommend", 
+  url: "/appProduct/sysRecommend" //商品推荐（首页）
 }, {
   name: "productClassify",
   url: "/appProduct/classify",  //获取一级和二级商品分类
--- src/views/home/tabs.vue
+++ src/views/home/tabs.vue
@@ -2,13 +2,13 @@
   <div class="row flex flex-wrap tabs-wrap">
     <div
       v-for="(item, index) in tabs"
-      :label="item.label"
-      :key="item.label+index"
+      :label="item.name"
+      :key="item.name"
       class="tabs-item flex-none"
-      @click="$router.push('/products/search?type='+item.label)"
+      @click="$router.push('/products/search?type='+item.name)"
     >
-      <img :src="require(`@/assets/img/${item.icon}`)" alt />
-      <h5>{{item.label}}</h5>
+      <img :src="item.img" alt />
+      <h5>{{item.name}}</h5>
     </div>
   </div>
 </template>
@@ -17,47 +17,12 @@
 export default {
   data() {
     return {
-      tabs: [{
-        label: '果蔬生鲜',
-        icon: 'tab01.png'
-      }, {
-        label: '肉类',
-        icon: 'tab02.png'
-      }, {
-        label: '干货坚果',
-        icon: 'tab03.png'
-      }, {
-        label: '家乡特产',
-        icon: 'tab04.png'
-      }, {
-        label: '速食',
-        icon: 'tab05.png'
-      }, {
-        label: '中药补品',
-        icon: 'tab06.png'
-      }, {
-        label: '海产',
-        icon: 'tab07.png'
-      }, {
-        label: '花卉苗木',
-        icon: 'tab08.png'
-      }, {
-        label: '粮油米面',
-        icon: 'tab09.png'
-      }, {
-        label: '农作艺品',
-        icon: 'tab10.png'
-      }, {
-        label: '更多',
-        icon: 'tab11.png'
-      }]
+      tabs: []
     }
   },
-  methods: {
-    clickHandler(label) {
-      // if you clicked home tab, then print 'Home'
-      console.log(label)
-    }
+  async created() {
+    let res = await this.$api.Product.productType();
+    this.tabs = res.list;
   }
 }
 </script>
