ccba1513c87981d6e3cb601eee97c1db40834f71
Vincentyun <vincentzyc@qq.com>
2019-07-23 11:32:13 +0800

    配置优化

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,17 @@ module.exports = {
   chainWebpack: config => {
     config.plugins.delete('prefetch')
   },
+  // configureWebpack: {
+  //   // 把webpack的配置写在这里 会自动合并
+  //   // 以下库使用cdn，不会被打包
+  //   externals: {
+  //     vue: "Vue",
+  //     vuex: "Vuex",
+  //     "vue-router": "VueRouter",
+  //     axios: "axios"
+  //   },
+  //   devtool: 'source-map'
+  // },
   css: {
     loaderOptions: {
       stylus: {
