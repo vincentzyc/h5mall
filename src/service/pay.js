1c713f46dab7f4a5eac4b857210dfda9762caf0b
Vincentyun <vincentzyc@qq.com>
2019-08-25 14:34:41 +0800

    pay

---
+++ src/service/pay.js
@@ -0,0 +1,17 @@
+// import Vue from "vue";
+// import router from "@/router"
+
+// const vm = new Vue()
+/**
+ * 微信支付
+ */
+export function wxpay() {
+  // return new Promise(resolve => {
+  //   if (!vm.USERINFO) {
+  //     let userInfo = vm.$util.getLStorage('userInfo', true);
+  //     if (!userInfo) return redirect ? router.replace('/me/login?redirect=' + redirect) : resolve(false);
+  //     vm.USERINFO = userInfo;
+  //   }
+  //   return resolve(vm.USERINFO);
+  // })
+}
