6934345be177f270d23f95f9617a69a44acccc5b
vincentzyc <vincentzyc@qq.com>
2019-09-08 12:41:05 +0800

    orderDetail

--- src/api/order.js
+++ src/api/order.js
@@ -1,19 +1,22 @@
 export default [{
-  name: "directSettlement",
-  url: "/znOrder/directSettlement" //立即支付（商品详情页）
-},{
-  name:"orderCard",
-  url:"/znOrder/orderCard"  //订单中可使用优惠券
-},{
-  name:"orderSubmit",
-  url:"/znOrder/orderSubmit"  //订单提交(即点击“支付”)
-},{
-  name:"allOrder",
-  url:"/znOrder/allOrder"    //所有订单（我的订单（包含待付款、发货、评价等））
-},{
-  name:"myNeedToDoOrder",
-  url:"/appUser/myNeedToDoOrder"  //各类订单数量
-},{
-  name:"appriseProduct",
-  url:"/znOrder/appriseProduct"  //评价商品
+	name: "directSettlement",
+	url: "/znOrder/directSettlement" //立即支付（商品详情页）
+}, {
+	name: "orderCard",
+	url: "/znOrder/orderCard" //订单中可使用优惠券
+}, {
+	name: "orderSubmit",
+	url: "/znOrder/orderSubmit" //订单提交(即点击“支付”)
+}, {
+	name: "allOrder",
+	url: "/znOrder/allOrder" //所有订单（我的订单（包含待付款、发货、评价等））
+}, {
+	name: "myNeedToDoOrder",
+	url: "/appUser/myNeedToDoOrder" //各类订单数量
+}, {
+	name: "appriseProduct",
+	url: "/znOrder/appriseProduct" //评价商品
+}, {
+	name: "orderDetail",
+	url: "/znOrder/orderDetail" //订单详情
 }]
