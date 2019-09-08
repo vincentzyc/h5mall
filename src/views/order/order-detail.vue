f583b176b6cf0bf98641b875a635a688ce4b2a87
vincentzyc <vincentzyc@qq.com>
2019-09-08 12:41:16 +0800

    orderDetail

--- src/views/order/order-detail.vue
+++ src/views/order/order-detail.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
+  <base-page>
     <common-header title="订单详情" />
-  </div>
+    <div>订单详情</div>
+  </base-page>
 </template>
+
+<script>
+import { getUser } from "@/service/user"
+export default {
+  async created() {
+    this.userInfo = await getUser(this.$route.fullPath);
+    this.order_id = this.$route.query.id;
+    if (!this.order_id) this.$router.back()
+  }
+}
+</script>
+
+<style>
+</style>
