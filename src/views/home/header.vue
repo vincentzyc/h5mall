6e3d5c02a879a6fd0f31e983b563119db4da6011
Vincentyun <vincentzyc@qq.com>
2019-09-11 10:57:54 +0800

    去除城市

--- src/views/home/header.vue
+++ src/views/home/header.vue
@@ -1,10 +1,10 @@
 <template>
   <header class="header-bg">
     <div class="flex fs14 cfff">
-      <div class="address pd-r10 pd-l15">
+      <!-- <div class="address pd-l15">
         <span>广州市</span>
-      </div>
-      <div class="search-wrap flex flex-auto flex-center pd-r10">
+      </div> -->
+      <div class="search-wrap flex flex-auto flex-center pd-l10 pd-r10">
         <div class="search-input flex flex-auto" @click="$router.push('/home/search')">
           <i class="cubeic-search"></i>
           <input type="text" placeholder="搜索" class="flex-auto" disabled>
