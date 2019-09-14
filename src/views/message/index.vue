ef198d40bbc24dcea7cb60701b8a83fba3e526b3
vincentzyc <vincentzyc@qq.com>
2019-09-14 22:24:58 +0800

    message

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -4,26 +4,34 @@
     <cube-scroll ref="scroll">
       <div class="scroll-wrapper">
         <ul class="msg-list">
-          <li class="flex align-middle">
-            <div class="icon icon1"></div>
+          <router-link to="/message/system" tag="li" class="flex align-middle">
+            <div class="icon icon1">
+              <span class="state" v-if="SystemMessage"></span>
+            </div>
             <span class="flex-auto">系统消息</span>
             <i class="cubeic-arrow c666"></i>
-          </li>
-          <li class="flex align-middle">
-            <div class="icon icon2"></div>
+          </router-link>
+          <router-link to="/message/trade" tag="li" class="flex align-middle">
+            <div class="icon icon2">
+              <span class="state" v-if="TradeMessage"></span>
+            </div>
             <span class="flex-auto">交易消息</span>
             <i class="cubeic-arrow c666"></i>
-          </li>
-          <li class="flex align-middle">
-            <div class="icon icon3"></div>
+          </router-link>
+          <router-link to="/message/purchase" tag="li" class="flex align-middle">
+            <div class="icon icon3">
+              <span class="state" v-if="PurchaseMessage"></span>
+            </div>
             <span class="flex-auto">农户报价</span>
             <i class="cubeic-arrow c666"></i>
-          </li>
-          <li class="flex align-middle">
-            <div class="icon icon4"></div>
+          </router-link>
+          <router-link to="/message/shop" tag="li" class="flex align-middle">
+            <div class="icon icon4">
+              <span class="state" v-if="ShopMessage"></span>
+            </div>
             <span class="flex-auto">农户通知</span>
             <i class="cubeic-arrow c666"></i>
-          </li>
+          </router-link>
         </ul>
       </div>
     </cube-scroll>
@@ -31,30 +39,40 @@
 </template>
 
 <script>
-
+import { getUser } from "@/service/user"
 export default {
   name: "message",
   data() {
     return {
-      items: [
-
-      ]
+      userInfo: "",
+      SystemMessage: false,
+      TradeMessage: false,
+      PurchaseMessage: false,
+      ShopMessage: false,
     }
   },
   methods: {
-
+    async getMessage() {
+      let param = {
+        user_id: this.userInfo.id,
+        token: this.userInfo.token
+      }
+      let res = await this.$api.Message.messageIndex(param);
+      this.SystemMessage = res.SystemMessage;
+      this.TradeMessage = res.TradeMessage;
+      this.PurchaseMessage = res.PurchaseMessage;
+      this.ShopMessage = res.ShopMessage;
+    }
   },
-  mounted() {
-    // this.$nextTick(function () {
-    //   setTimeout(() => {
-    //     this.$refs.slide.refresh();
-    //     this.$refs.scroll.refresh();
-    //   }, 0);
-    // });
+  async created() {
+    this.userInfo = await getUser(this.$route.fullPath);
+    this.getMessage()
   }
 };
 </script>
 <style lang="stylus" scoped>
+@import '~@/assets/css/color.styl';
+
 .msg-list {
   background: #fff;
   margin-bottom: 15px;
@@ -66,9 +84,21 @@ export default {
   }
 
   .icon {
+    position: relative;
     width: 30px;
     height: 30px;
     margin-right: 5px;
+  }
+
+  .state {
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    color: #fff;
+    border-radius: 5px;
+    background: $color-theme;
+    width: 8px;
+    height: 8px;
   }
 
   .icon1 {
