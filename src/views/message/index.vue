82cfc056622a716b78d974f8ba3530c6e33973fb
vincentzyc <vincentzyc@qq.com>
2019-09-14 21:14:49 +0800

    message

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -1,26 +1,39 @@
 <template>
   <base-page>
+    <common-header title="消息" hideBack></common-header>
     <cube-scroll ref="scroll">
       <div class="scroll-wrapper">
-        <div class="text-center mg20">消息</div>
-        <div style="height:1500px;width:100%;background:#999"></div>
-        <div class="text-center mg20">到底啦</div>
+        <ul class="msg-list">
+          <li class="flex align-middle">
+            <div class="icon icon1"></div>
+            <span class="flex-auto">系统消息</span>
+            <i class="cubeic-arrow c666"></i>
+          </li>
+          <li class="flex align-middle">
+            <div class="icon icon2"></div>
+            <span class="flex-auto">交易消息</span>
+            <i class="cubeic-arrow c666"></i>
+          </li>
+          <li class="flex align-middle">
+            <div class="icon icon3"></div>
+            <span class="flex-auto">农户报价</span>
+            <i class="cubeic-arrow c666"></i>
+          </li>
+          <li class="flex align-middle">
+            <div class="icon icon4"></div>
+            <span class="flex-auto">农户通知</span>
+            <i class="cubeic-arrow c666"></i>
+          </li>
+        </ul>
       </div>
     </cube-scroll>
   </base-page>
 </template>
 
 <script>
-// @ is an alias to /src
-// import vHeader from "./header.vue";
-// import vFooter from "./footer.vue";
 
 export default {
   name: "message",
-  components: {
-    // 'v-header': vHeader,
-    // 'v-footer': vFooter
-  },
   data() {
     return {
       items: [
@@ -41,3 +54,37 @@ export default {
   }
 };
 </script>
+<style lang="stylus" scoped>
+.msg-list {
+  background: #fff;
+  margin-bottom: 15px;
+  padding: 0 10px;
+
+  li {
+    padding: 12px 5px;
+    border-bottom: 1px solid #eee;
+  }
+
+  .icon {
+    width: 30px;
+    height: 30px;
+    margin-right: 5px;
+  }
+
+  .icon1 {
+    background: url('~@/assets/img/message.png') no-repeat 0px 0px / auto 100%;
+  }
+
+  .icon2 {
+    background: url('~@/assets/img/message.png') no-repeat -30px 0px / auto 100%;
+  }
+
+  .icon3 {
+    background: url('~@/assets/img/message.png') no-repeat -60px 0px / auto 100%;
+  }
+
+  .icon4 {
+    background: url('~@/assets/img/message.png') no-repeat -90px 0px / auto 100%;
+  }
+}
+</style>
