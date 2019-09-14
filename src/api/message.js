66ed646b1393c0ab2b69a1decfbc9810f63b1630
vincentzyc <vincentzyc@qq.com>
2019-09-14 20:30:52 +0800

    message

---
+++ src/api/message.js
@@ -0,0 +1,16 @@
+export default [{
+	name: "messageIndex",
+	url: "/appMessage/messageIndex" // 消息首页
+}, {
+	name: "systemMessage",
+	url: "/appMessage/systemMessage" // 系统消息
+}, {
+	name: "tradeMessage",
+	url: "/appMessage/tradeMessage" // 交易消息
+}, {
+	name: "purchaseMessage",
+	url: "/appMessage/purchaseMessage" // 报价消息
+}, {
+	name: "shopMessage",
+	url: "/appMessage/shopMessage" // 商户消息（农户通知）
+}]
