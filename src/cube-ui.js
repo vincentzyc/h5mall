9dff449672ff210717ba923caadb47211ba2d988
vincentzyc <vincentzyc@qq.com>
2019-09-15 12:10:56 +0800

    add RecycleList

--- src/cube-ui.js
+++ src/cube-ui.js
@@ -46,6 +46,7 @@ import {
   // Sticky,
   // ScrollNav,
   // ScrollNavBar
+  RecycleList
 } from "cube-ui";
 
 Vue.use(Button);
@@ -84,3 +85,5 @@ Vue.use(Slide)
 // Vue.use(Sticky)
 // Vue.use(ScrollNav)
 // Vue.use(ScrollNavBar)
+Vue.use(RecycleList)
+
