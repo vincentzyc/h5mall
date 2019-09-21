1b444d80b7de0f68856b0abbcc98156d849b8348
vincentzyc <vincentzyc@qq.com>
2019-09-21 17:46:47 +0800

    publish

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,24 +5,26 @@
     </common-header>
     <cube-scroll ref="scroll">
       <div class="scroll-wrapper">
-        <div class="bgfff">
-          <cube-tab-bar v-model="selectedLabel" show-slider class="border-beee">
+        <div>
+          <cube-tab-bar v-model="selectedLabel" show-slider class="border-beee bgfff">
             <cube-tab v-for="(item, index) in tabs" :label="item" :key="item+index" class="lh30 width100"></cube-tab>
           </cube-tab-bar>
           <cube-tab-panels class="mg-b10">
             <cube-tab-panel class="img-panel" label="采购中" v-show="selectedLabel==='采购中'">
-              <ul>
+              <ul v-if="purchasing.length>0">
                 <li>111</li>
                 <li>111</li>
                 <li>时间：{{$util.getFormatDate('yyyy-mm-dd')}}</li>
               </ul>
+              <p v-else class="mg20 pd20 text-center">暂无采购</p>
             </cube-tab-panel>
             <cube-tab-panel class="text-panel" label="已停止" v-show="selectedLabel==='已停止'">
-              <ul>
+              <ul v-if="purchaseStop.length>0">
                 <li>22</li>
                 <li>222</li>
                 <li>时间：{{$util.getFormatDate('yyyy-mm-dd')}}</li>
               </ul>
+              <p v-else class="mg20 pd20 text-center">暂无采购</p>
             </cube-tab-panel>
           </cube-tab-panels>
         </div>
--- src/views/publish/publish.vue
+++ src/views/publish/publish.vue
@@ -1,8 +1,63 @@
 <template>
   <base-page>
-    <common-header title="发布采购"/>
+    <common-header title="发布采购" />
     <div>
-      发布采购
+      <div class="common-form mg-t10">
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">采购的物品：</label>
+          <base-input-item class="input-item flex-auto" type="text" v-model="formData.name" placeholder="请输入采购物品名称"></base-input-item>
+        </div>
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">需求分类：</label>
+          <div class="flex input-item flex-auto">
+            <select class="flex-none mg-r10 col-6" v-model="formData.type">
+              <option v-for="item in classify" :key="item.parent_id" :value="item.name">{{item.name}}</option>
+            </select>
+            <select class="flex-none col-6" v-model="formData.type_id">
+              <option v-for="item in classify2" :key="item.classify_id" :value="item.classify_id">{{item.classifyName}}</option>
+            </select>
+          </div>
+        </div>
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">货源地：</label>
+          <base-input-item
+            class="input-item flex-auto"
+            type="text"
+            v-model="formData.from_address"
+            placeholder="请输入期望的货源地"
+          ></base-input-item>
+        </div>
+      </div>
+
+      <div class="common-form mg-t10">
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">数量：</label>
+          <base-input-item class="input-item flex-auto" type="digit" v-model="formData.num" placeholder="请输入数量"></base-input-item>
+        </div>
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">单位：</label>
+          <base-input-item class="input-item flex-auto" type="digit" v-model="formData.unit" placeholder="请输入单位"></base-input-item>
+        </div>
+        <div class="flex align-middle pd-l15 pd5 border-beee">
+          <label class="flex-none input-title">预算金额：</label>
+          <base-input-item class="input-item flex-auto" type="money" v-model="formData.money" placeholder="请输入预算金额"></base-input-item>
+        </div>
+      </div>
+      <div class="common-form mg-t10">
+        <div class="pd15">
+          <label>需求描述：</label>
+          <cube-textarea
+            v-model.trim="formData.note"
+            placeholder="详细描述采购要求，可以收到更满意的报价哦"
+            :maxlength="200"
+            class="mg-t10"
+            style="min-height: 100px"
+          ></cube-textarea>
+        </div>
+      </div>
+      <div class="flex pd20">
+        <cube-button :primary="true" @click="submit()">发布</cube-button>
+      </div>
     </div>
   </base-page>
 </template>
@@ -14,10 +69,31 @@ export default {
   data() {
     return {
       userInfo: "",
+      classify: [],
+      formData: {
+        name: "",
+        type: "",
+        type_id: "",
+        from_address: "",
+        unit: "",
+        num: "",
+        money: "",
+        note: "",
+        code: "",
+        img: ""
+      },
       selectedLabel: '采购中',
       tabs: ['采购中', '已停止'],
       purchasing: [],
       purchaseStop: []
+    }
+  },
+  computed: {
+    classify2() {
+      if (this.classify.length === 0) return [];
+      let arr = this.classify.find(v => v.name === this.formData.type);
+      if (Array.isArray(arr.classificationTwo) && arr.classificationTwo.length > 0) this.formData.type_id = arr.classificationTwo[0].classify_id;
+      return arr.classificationTwo || []
     }
   },
   methods: {
@@ -36,10 +112,17 @@ export default {
       }
       let res = await this.$api.Publish.purchaseStop(param);
       console.log('已停止', res);
-    }
+    },
+    async getClassify() {
+      let res = await this.$api.Product.productClassify('');
+      this.classify = res.classify || [];
+      if (Array.isArray(res.classify)) this.formData.type = this.classify[0].name;
+    },
+    submit() { }
   },
   async created() {
     this.userInfo = await getUser(this.$route.fullPath);
+    this.getClassify()
   }
 };
 </script>
