ea29857e1f966a44a7f396f15590ee0de04900a1
vincentzyc <vincentzyc@qq.com>
2019-09-21 15:28:25 +0800

    publish api

---
+++ src/api/publish.js
@@ -0,0 +1,25 @@
+export default [{
+	name: "publish",
+	url: "/appPurchase/publish" // 发布需求
+}, {
+	name: "purchasing",
+	url: "/appPurchase/purchasing" // 采购中的采购
+}, {
+	name: "purchaseStop",
+	url: "/appPurchase/purchaseStop" // 已停止的采购
+}, {
+	name: "stopPurchase",
+	url: "/appPurchase/stopPurchase" // 停止采购
+}, {
+	name: "purchaseUpdate",
+	url: "/appPurchase/purchaseUpdate" // 修改采购
+}, {
+	name: "purchaseInfo",
+	url: "/appPurchase/purchaseInfo" //  采购详情
+}, {
+	name: "quote",
+	url: "/appQuote/quote" //  发布报价
+}, {
+	name: "quoteInfo",
+	url: "/appQuote/quoteInfo" //  报价详情
+}]
