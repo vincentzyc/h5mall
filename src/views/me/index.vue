3ebaad0bcd18a9bf7b5b164f794e6c7850a30b37
vincentzyc <vincentzyc@qq.com>
2019-09-24 23:15:52 +0800

    代码整理

--- src/views/me/index.vue
+++ src/views/me/index.vue
@@ -47,11 +47,11 @@
         </ul>
       </div>
       <ul class="info-list">
-        <!-- <li class="flex align-middle">
+        <li class="flex align-middle">
           <div class="icon icon1"></div>
           <span class="flex-auto">钱包：</span>
           <strong class="ctheme">￥{{user.user_money||0}}</strong>
-        </li> -->
+        </li>
         <li class="flex align-middle" @click="$router.push('/me/likestore')">
           <div class="icon icon2"></div>
           <span class="flex-auto">我关注的商家</span>
@@ -79,42 +79,14 @@
           <span class="flex-auto">设置</span>
           <i class="cubeic-arrow c666"></i>
         </li>
-        <li class="flex align-middle" @click="$refs.followPopup.show()">
-          <div class="icon icon7"></div>
-          <span class="flex-auto">关注公众号</span>
-          <i class="cubeic-arrow c666"></i>
-        </li>
       </ul>
     </cube-scroll>
 
-    <cube-popup position="center" center :mask-closable="true" ref="followPopup">
-      <div class="follow-popup">
-        <div class="frame">
-          <div class="frame_title">关注公众号</div>
-          <div class="frame_mode_first">方式一：微信扫描或识别关注卡</div>
-          <div class="frame_mode_first_btn" @click="$refs.pagePopup.open();$refs.followPopup.hide()">获取关注卡</div>
-          <div class="frame_mode_first_border"></div>
-
-          <div class="frame_mode_first" style="margin-top: 20px">方式 二：复制链接到微信搜索</div>
-          <div class="frame_mode_second_text">{{gzhLink}}</div>
-          <div class="frame_mode_first_btn" @click="copy()">复制链接</div>
-        </div>
-      </div>
-    </cube-popup>
-    <!-- 识别关注卡 -->
-    <page-popup ref="pagePopup" position="right" class="pd-t44" type="getgzh">
-      <common-header title="关注卡" />
-      <div class="gzh-bg">
-        <img src="@/assets/img/gzh.jpg" alt width="100%" />
-        <p class="text-center mg-t10">长按图片识别二维码即可关注掌农公众号</p>
-      </div>
-    </page-popup>
   </base-page>
 </template>
 
 <script>
 import { getUser, updateUser } from "@/service/user"
-import Clipboard from "@/utils/clipboard"
 
 export default {
   name: "me",
@@ -123,27 +95,11 @@ export default {
   data() {
     return {
       user: "",
-      contactPhone: "18827737585",
-      items: [],
-      gzhLink: "https://word=%E7%AB%99%E9%85%B7%E7%BD%91&tn=25017023_10_pg&lm=-1&ssl_s=1&ssl_c=ssl1_16733da497c"
+      contactPhone: "18826229916",
+      items: []
     }
   },
   methods: {
-    copy() {
-      let clipboard = new Clipboard();
-      clipboard.copy(this.gzhLink, () => {
-        this.$createToast({
-          txt: '复制成功',
-          type: 'txt',
-          time: 2000
-        }).show()
-      }, () => {
-        this.$createDialog({
-          type: 'alert',
-          content: '您的浏览器不支持自动复制，请手动复制',
-        }).show()
-      });
-    },
     contact() {
       this.$createDialog({
         type: 'confirm',
@@ -328,64 +284,6 @@ export default {
   .icon7 {
     background: url('~@/assets/img/me.png') no-repeat 0 -1px / auto 100%;
   }
-}
-
-.follow-popup {
-  position: relative;
-  background: #fff;
-  padding: 15px;
-  border-radius: 10px;
-  width: 80%;
-  margin: 0 auto;
-
-  .frame {
-    padding: 10px;
-  }
-
-  .frame_title {
-    font-size: 16px;
-    color: #333333;
-    font-weight: bold;
-    background-size: 18px;
-  }
-
-  .frame_mode_first {
-    font-size: 16px;
-    color: #333333;
-    font-weight: bold;
-    margin: 15px 0;
-  }
-
-  .frame_mode_first_btn {
-    font-size: 14px;
-    color: #ffffff;
-    background: #fc3c47;
-    width: 100px;
-    height: 28px;
-    border-radius: 14px;
-    text-align: center;
-    line-height: 28px;
-  }
-
-  .frame_mode_first_border {
-    height: 1px;
-    border-bottom: 1px dashed #d2d2d2;
-    margin: 15px 0;
-  }
-
-  .frame_mode_second_text {
-    font-size: 12px;
-    color: #666666;
-    word-break: break-all;
-    margin: 15px 0;
-  }
-}
-
-.gzh-bg {
-  width: 100%;
-  height: 100%;
-  padding: 10%;
-  background: url('~@/assets/img/gzh-bg.jpg') no-repeat 0 0 / 100% 100%;
 }
 </style>
 
