77fbb4f475cb86b018fa59317663e33b321693e4
Vincentyun <vincentzyc@qq.com>
2019-10-15 11:35:01 +0800

    fix bug

--- src/api/order.js
+++ src/api/order.js
@@ -1,22 +1,31 @@
 export default [{
-	name: "directSettlement",
-	url: "/znOrder/directSettlement" //立即支付（商品详情页）
+  name: "directSettlement",
+  url: "/znOrder/directSettlement" //立即支付（商品详情页）
 }, {
-	name: "orderCard",
-	url: "/znOrder/orderCard" //订单中可使用优惠券
+  name: "orderCard",
+  url: "/znOrder/orderCard" //订单中可使用优惠券
 }, {
-	name: "orderSubmit",
-	url: "/znOrder/orderSubmit" //订单提交(即点击“支付”)
+  name: "orderSubmit",
+  url: "/znOrder/orderSubmit" //订单提交(即点击“支付”)
 }, {
-	name: "allOrder",
-	url: "/znOrder/allOrder" //所有订单（我的订单（包含待付款、发货、评价等））
+  name: "allOrder",
+  url: "/znOrder/allOrder" //所有订单（我的订单（包含待付款、发货、评价等））
 }, {
-	name: "myNeedToDoOrder",
-	url: "/appUser/myNeedToDoOrder" //各类订单数量
+  name: "myNeedToDoOrder",
+  url: "/appUser/myNeedToDoOrder" //各类订单数量
 }, {
-	name: "appriseProduct",
-	url: "/znOrder/appriseProduct" //评价商品
+  name: "appriseProduct",
+  url: "/znOrder/appriseProduct" //评价商品
 }, {
-	name: "orderDetail",
-	url: "/znOrder/orderDetail" //订单详情
+  name: "orderDetail",
+  url: "/znOrder/orderDetail" //订单详情
+}, {
+  name: "confirmReceipt",
+  url: "/znOrder/confirmReceipt"  //用户确认收货
+}, {
+  name: "goPay",
+  url: "/znOrder/goPay"  //待付款订单点击去支付按钮
+}, {
+  name: "orderSubmitOfWait",
+  url: "/znOrder/orderSubmitOfWait"  //待付款订单提交
 }]
