c8a3cce12c1c1bcaa2df99823c749d148581c559
Vincentyun <vincentzyc@qq.com>
2019-10-18 18:57:11 +0800

    wechat pay

--- src/views/order/order-detail.vue
+++ src/views/order/order-detail.vue
@@ -1,93 +1,91 @@
 <template>
   <base-page>
     <common-header title="订单详情" />
-    <div class="content" :class="{'pd-b50':footerStatus}">
-      <cube-scroll ref="scroll">
-        <div class="scroll-wrapper" v-if="detail">
-          <div class="flex align-middle bgfff pd10">
-            <div class="flex-auto cblue">{{detail.order_status|orderStatus}}</div>
-            <img v-if="detail.order_status===0" src="@/assets/img/dfk.png" alt="待付款" class="width80" />
-            <img v-if="detail.order_status===1" src="@/assets/img/dfh.png" alt="待发货" class="width80" />
-            <img v-if="detail.order_status===2" src="@/assets/img/dsh.png" alt="待收货" class="width80" />
-            <img v-if="detail.order_status===3" src="@/assets/img/dpj.png" alt="待评价" class="width80" />
-          </div>
+    <div class="content">
+      <div class="scroll-wrapper" v-if="detail" :class="{'pd-b50':footerStatus}">
+        <div class="flex align-middle bgfff pd10">
+          <div class="flex-auto cblue">{{detail.order_status|orderStatus}}</div>
+          <img v-if="detail.order_status===0" src="@/assets/img/dfk.png" alt="待付款" class="width80" />
+          <img v-if="detail.order_status===1" src="@/assets/img/dfh.png" alt="待发货" class="width80" />
+          <img v-if="detail.order_status===2" src="@/assets/img/dsh.png" alt="待收货" class="width80" />
+          <img v-if="detail.order_status===3" src="@/assets/img/dpj.png" alt="待评价" class="width80" />
+        </div>
 
-          <div class="address-wrap bgfff mg-t10">
-            <div class="flex align-middle address">
-              <i class="c999 cubeic-location"></i>
-              <div class="flex flex-auto flex-column pd10">
-                <div class="flex flex-auto">
-                  <p class="flex-auto textover1">收货人：{{detail.consignee_name}}</p>
-                  <p class="flex-none textover1">{{detail.consignee_phone}}</p>
-                </div>
-                <p class="fs14 c666 mg-t10 textover2 lh20">{{detail.consignee_address}}</p>
+        <div class="address-wrap bgfff mg-t10">
+          <div class="flex align-middle address">
+            <i class="c999 cubeic-location"></i>
+            <div class="flex flex-auto flex-column pd10">
+              <div class="flex flex-auto">
+                <p class="flex-auto textover1">收货人：{{detail.consignee_name}}</p>
+                <p class="flex-none textover1">{{detail.consignee_phone}}</p>
               </div>
+              <p class="fs14 c666 mg-t10 textover2 lh20">{{detail.consignee_address}}</p>
             </div>
-            <img class="flex-none" src="@/assets/img/order-address.png" alt width="100%" />
           </div>
+          <img class="flex-none" src="@/assets/img/order-address.png" alt width="100%" />
+        </div>
 
-          <div class="products mg-t10 bgfff" v-for="store in orderList" :key="store.shop_id">
-            <div class="flex align-middle pd10 c666 bgfff">
-              <span class="store"></span>
-              <div class="flex align-middle mg-l10">
-                <span class="mg-r10">{{store.shop_name}}</span>
-                <i class="fs16 cubeic-arrow"></i>
-              </div>
+        <div class="products mg-t10 bgfff" v-for="store in orderList" :key="store.shop_id">
+          <div class="flex align-middle pd10 c666 bgfff">
+            <span class="store"></span>
+            <div class="flex align-middle mg-l10">
+              <span class="mg-r10">{{store.shop_name}}</span>
+              <i class="fs16 cubeic-arrow"></i>
             </div>
-            <div class="product-item bgeee mg-b5" v-for="item in store.products" :key="item.id">
-              <div class="pd10">
-                <div class="flex product-info">
-                  <img class="flex-none" :src="item.img.split(',')[0]" alt="店铺logo" />
-                  <div class="flex-auto pd-l10">
-                    <h4 class="textover2 lh20 title">{{item.product_name}}</h4>
-                    <div class="flex mg-t10">
-                      <span class="flex-auto ctheme fs16">￥{{item.price}}</span>
-                      <span class="mg5">x{{item.num}}</span>
-                    </div>
+          </div>
+          <div class="product-item bgeee mg-b5" v-for="item in store.products" :key="item.id">
+            <div class="pd10">
+              <div class="flex product-info">
+                <img class="flex-none" :src="item.img.split(',')[0]" alt="店铺logo" />
+                <div class="flex-auto pd-l10">
+                  <h4 class="textover2 lh20 title">{{item.product_name}}</h4>
+                  <div class="flex mg-t10">
+                    <span class="flex-auto ctheme fs16">￥{{item.price}}</span>
+                    <span class="mg5">x{{item.num}}</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="bgfff lh20 pd10 text-right">合计：￥{{orderTotalPrice}}</div>
+        </div>
+        <div class="bgfff lh20 pd10 text-right">合计：￥{{orderTotalPrice}}</div>
 
-          <div class="mg-t10 bgfff pd10">
-            <div class="c666 mg-b10">备注信息</div>
-            <div class="lh20 fs12">{{detail.remark}}</div>
-          </div>
+        <div class="mg-t10 bgfff pd10">
+          <div class="c666 mg-b10">备注信息</div>
+          <div class="lh20 fs12">{{detail.remark}}</div>
+        </div>
 
-          <div class="mg-t10 bgfff pd10 order-record">
-            <ul class="fs12 c666 lh24">
-              <li v-if="detail.create_time">下单时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.create_time)}}</li>
-              <li v-if="detail.order_no">订单号：{{detail.order_no}}</li>
-              <li v-if="detail.send_time">发货时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.send_time)}}</li>
-              <li v-if="detail.delivery_company">物流公司：{{detail.delivery_company}}</li>
-              <li v-if="detail.delivery_id">物流单号：{{detail.delivery_id}}</li>
-              <li v-if="detail.received_time">收货时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.received_time)}}</li>
-            </ul>
-          </div>
+        <div class="mg-t10 bgfff pd10 order-record">
+          <ul class="fs12 c666 lh24">
+            <li v-if="detail.create_time">下单时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.create_time)}}</li>
+            <li v-if="detail.order_no">订单号：{{detail.order_no}}</li>
+            <li v-if="detail.send_time">发货时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.send_time)}}</li>
+            <li v-if="detail.delivery_company">物流公司：{{detail.delivery_company}}</li>
+            <li v-if="detail.delivery_id">物流单号：{{detail.delivery_id}}</li>
+            <li v-if="detail.received_time">收货时间：{{$util.getFormatDate('yyyy-mm-dd hh:mm',detail.received_time)}}</li>
+          </ul>
+        </div>
 
-          <div class="mg-t10 bgfff">
-            <h3 class="pd10 lh20 fs14 c999 border-beee">请选择支付方式</h3>
-            <ul class="shop_ul">
-              <cube-radio-group v-model="selected">
-                <cube-radio option="wx" position="right" class="border-beee">
-                  <img src="@/assets/img/wx-pay.png" width="20px" height="20px" class="mg-r10" />
-                  微信支付
-                </cube-radio>
-                <cube-radio option="zfb" position="right" class="border-beee" v-if="!$util.isWechat()">
-                  <img src="@/assets/img/zfb-pay.png" width="20px" height="20px" class="mg-r10" />
-                  支付宝支付
-                </cube-radio>
-                <!-- <cube-radio option="ye" position="right" class="border-beee">
+        <div class="mg-t10 bgfff">
+          <h3 class="pd10 lh20 fs14 c999 border-beee">请选择支付方式</h3>
+          <ul class="shop_ul">
+            <cube-radio-group v-model="selected">
+              <cube-radio option="wx" position="right" class="border-beee">
+                <img src="@/assets/img/wx-pay.png" width="20px" height="20px" class="mg-r10" />
+                微信支付
+              </cube-radio>
+              <cube-radio option="zfb" position="right" class="border-beee" v-if="!$util.isWechat()">
+                <img src="@/assets/img/zfb-pay.png" width="20px" height="20px" class="mg-r10" />
+                支付宝支付
+              </cube-radio>
+              <!-- <cube-radio option="ye" position="right" class="border-beee">
               <img src="@/assets/img/balance.png" width="20px" height="20px" class="mg-r10" />
               余额支付
-                </cube-radio>-->
-              </cube-radio-group>
-            </ul>
-          </div>
+              </cube-radio>-->
+            </cube-radio-group>
+          </ul>
         </div>
-      </cube-scroll>
+      </div>
 
       <footer class="footerwrap bgtheme" v-if="footerStatus" @click="footerClick(detail.order_status)">{{footerStatus}}</footer>
     </div>
@@ -152,11 +150,20 @@ export default {
           return ''
       }
     },
-    pay() {
-      let orderParam = {
+    async pay() {
+      let param = {
         user_id: this.userInfo.id.toString(),
         token: this.userInfo.token,
-        order_id: this.detail.order_id
+        id_in: this.id_in,
+        card_id: this.detail.card_id,
+        name: this.detail.consignee_name,
+        phone: this.detail.consignee_phone,
+        address: this.detail.consignee_address
+      }
+      let res = await this.$api.Order.orderSubmitOfWait(param);
+      let orderParam = {
+        order_id: res.order_id,
+        ...this.userInfo
       }
       goPay(orderParam, this.selected)
     },
@@ -177,13 +184,8 @@ export default {
         token: this.userInfo.token,
         order_id: this.id_in
       }
-      console.log(param);
       let res = await this.$api.Order.orderDetail(param);
-      console.log(res);
       this.detail = res.order_detail;
-      setTimeout(() => {
-        this.$refs.scroll.refresh();
-      }, 66);
     }
   },
   async created() {
@@ -197,9 +199,19 @@ export default {
 
 <style lang="stylus" scoped>
 .content {
+  position: relative;
   width: 100%;
   height: 100%;
   overflow: hidden;
+}
+
+.scroll-wrapper {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: auto;
 }
 
 .address-wrap .address {
@@ -237,5 +249,6 @@ export default {
   line-height: 50px;
   color: #fff;
   font-size: 16px;
+  z-index: 99;
 }
 </style>
