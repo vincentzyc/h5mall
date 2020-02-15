db40f55e07854b065e63d714051b8f6632bca960
vincentzyc <vincentzyc@qq.com>
2020-02-15 15:32:45 +0800

    getIPAdress

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, HttpService } from '@nestjs/common';
 import { Logger } from './logger-service';
+import { getIPAdress } from './utils';
 
 @Injectable()
 export class AppService {
   constructor(private readonly httpService: HttpService) { }
 
   async getHello(): Promise<string> {
-    let res = await this.httpService.post('http://192.168.220.188:3030/bbb',{aaa:111,bbb:222,ccc:'123123'}).toPromise();
+    const remoteIp: string = getIPAdress();
+
+    let res = await this.httpService.post(`http://${remoteIp}:3030/bbb`, { aaa: 111, bbb: 222, ccc: '123123' }).toPromise();
     // let res = await this.httpService.get('http://192.168.220.188:3030/aaa/666').toPromise();
     Logger.info(`这是一条测试日志111///${JSON.stringify(res.data)}`)
     return JSON.stringify(res.data);
