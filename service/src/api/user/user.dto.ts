0ef215ffd7066bd65435d52c2c4504a390fce52d
vincentzyc <vincentzyc@qq.com>
2020-02-22 13:03:28 +0800

    UserDto

--- service/src/api/user/user.dto.ts
+++ service/src/api/user/user.dto.ts
@@ -1,14 +1,15 @@
-// import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UserDto {
-  // @ApiProperty({ required: true, description: '用户名' })
   @IsString({ message: '用户名必须为字符类型' })
   @IsNotEmpty({ message: '姓名不能为空' })
-  readonly username: string;
+  name: string;
 
-  // @ApiProperty({ required: true, description: '密码' })
+  @IsString({ message: '手机号必须为字符类型' })
+  @IsNotEmpty({ message: '手机号不能为空' })
+  readonly phone: string;
+
   @IsString({ message: '密码必须为字符串类型' })
   @IsNotEmpty({ message: '密码不能为空' })
-  readonly password: string;
+  password: string;
 }
