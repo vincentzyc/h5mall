a8a62ac5e0ef68b9bbae265c6ce0de4ac69b6b2a
vincentzyc <vincentzyc@qq.com>
2020-02-23 23:12:36 +0800

    userDto

--- service/src/api/user/user.dto.ts
+++ service/src/api/user/user.dto.ts
@@ -1,13 +1,13 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UserDto {
-  @IsString({ message: '用户名必须为字符类型' })
-  @IsNotEmpty({ message: '姓名不能为空' })
-  name: string;
+  // @IsString({ message: '用户名必须为字符类型' })
+  // @IsNotEmpty({ message: '姓名不能为空' })
+  // name: string;
 
   @IsString({ message: '手机号必须为字符类型' })
   @IsNotEmpty({ message: '手机号不能为空' })
-  readonly phone: string;
+  phone: string;
 
   @IsString({ message: '密码必须为字符串类型' })
   @IsNotEmpty({ message: '密码不能为空' })
