18259b2735564dac105786609dc7d78d83084493
Vincentyun <vincentzyc@qq.com>
2020-02-26 17:47:53 +0800

    HttpExceptionFilter

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局注册错误的过滤器(错误异常)
+  app.useGlobalFilters(new HttpExceptionFilter());
   // 全局注册拦截器(成功返回格式)
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3030);
