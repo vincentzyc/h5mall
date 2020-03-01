3f58b5df8b5b03cf954379ba9806fde62ec98a19
vincentzyc <vincentzyc@qq.com>
2020-03-01 20:40:53 +0800

    ValidationPipe

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from './pipe/validation.pipe';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   // 全局注册拦截器(成功返回格式)
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 全局使用管道(数据校验)
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3030);
 }
 bootstrap();
