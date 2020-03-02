cc4e132e273f2899c4851728779c9c522bc8f840
Vincentyun <vincentzyc@qq.com>
2020-03-02 17:03:19 +0800

    ValidationPipe

--- service/src/pipe/validation.pipe.ts
+++ service/src/pipe/validation.pipe.ts
@@ -5,7 +5,6 @@ import {
   ArgumentMetadata,
   Injectable,
   PipeTransform,
-  Logger,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
@@ -15,39 +14,30 @@ import * as _ from 'lodash';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
-  async transform(value: any, metadata: ArgumentMetadata) {
-    const { metatype } = metadata;
-    console.log(value,JSON.stringify(metatype));
-    
+  async transform(value: any, { metatype }: ArgumentMetadata) {
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
-    console.log(errors);
-    
     if (errors.length > 0) {
       // 遍历全部的错误信息,返回给前端
-      const errorMessage = errors.map(item => {
-        return {
-          message: '参数验证错误',
-          currentValue: item.value === undefined ? '' : item.value,
-          [item.property]: _.values(item.constraints)[0],
-        };
-      });
+      // const errorMessage = errors.map(item => {
+        // return {
+        //   message: '参数验证错误',
+        //   currentValue: item.value === undefined ? '' : item.value,
+        //   [item.property]: _.values(item.constraints)[0],
+        // };
+      // });
+      const errorMessage = _.values(errors[0].constraints)[0]
       // 统一抛出异常
-      throw new HttpException(
-        { message: errorMessage },
-        HttpStatus.OK,
-      );
+      throw new HttpException(errorMessage, HttpStatus.OK);
     }
     return value;
   }
 
-  private toValidate(metatype: any): boolean {
-    const types = [String, Boolean, Number, Array, Object];
-    console.log(types.includes(metatype));
-    
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
   }
 }
