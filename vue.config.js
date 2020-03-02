d638c5fc464bdb2dd5c00ffaede7ef5b2308c702
Vincentyun <vincentzyc@qq.com>
2020-03-02 17:08:38 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   devServer: {
     port: 3020,
-    proxy: 'http://192.168.220.188:3030/'
+    proxy: 'https://www.xxx.com'
   },
   chainWebpack: config => {
     config.plugins.delete('prefetch')
