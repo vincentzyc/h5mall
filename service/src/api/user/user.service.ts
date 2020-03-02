fdc22c9a7285dd96251e46140c3f9c7be0acc7be
Vincentyun <vincentzyc@qq.com>
2020-03-02 17:04:32 +0800

    UserService

--- service/src/api/user/user.service.ts
+++ service/src/api/user/user.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity, UserResult } from './user.entity';
 import { UserDto } from './user.dto';
-import { validate } from 'class-validator';
 import * as _ from 'lodash';
 
 interface UserQuery {
@@ -30,69 +29,18 @@ export class UserService {
     };
   }
   async appUserReg(userDto: UserDto): Promise<any> {
-    let userPost = new UserDto();
-    userPost.phone = userDto.phone;
-    userPost.password = userDto.password;
-    const errors = await validate(userPost);
-    if (errors.length > 0) {
-      // 遍历全部的错误信息,返回给前端
-      const errorMessage = errors.map(item => {
-        return {
-          message: '参数验证错误',
-          currentValue: item.value === undefined ? '' : item.value,
-          [item.property]: _.values(item.constraints)[0],
-        };
-      });
-      // 返回异常
-      console.log(JSON.stringify(errorMessage[0]));
-      return JSON.stringify(errorMessage[0]);
-    } else {
-      let result: Number = await this.userRepository.count({ phone: userPost.phone });
-      if (result) throw new HttpException('用户已存在', HttpStatus.OK);
-      return await this.userRepository.save(userDto);
-    }
+    const { phone } = userDto;
+    let result: Number = await this.userRepository.count({ phone: phone });
+    if (result) throw new HttpException('用户已存在', HttpStatus.OK);
+    return await this.userRepository.save(userDto);
   }
   async appUserLogin(userDto: UserDto): Promise<any> {
-    let userPost = new UserDto();
-    userPost.phone = userDto.phone;
-    userPost.password = userDto.password;
-    const errors = await validate(userPost);
-    if (errors.length > 0) {
-      // 遍历全部的错误信息,返回给前端
-      const errorMessage = errors.map(item => {
-        return {
-          message: '参数验证错误',
-          currentValue: item.value === undefined ? '' : item.value,
-          [item.property]: _.values(item.constraints)[0],
-        };
-      });
-      // 返回异常
-      console.log(JSON.stringify(errorMessage[0]));
-      return JSON.stringify(errorMessage[0]);
-    } else {
-      console.log("validation succeed");
-      let findUser = await this.userRepository.findOne({ phone: userPost.phone });
-      if (Object.keys(findUser).length > 0) {
-        if (findUser.password === userPost.password) return findUser
-        // return '密码错误';
-        throw new HttpException('密码错误', HttpStatus.OK);
-      };
-      return '用户未注册';
+    const { phone, password } = userDto;
+    let findUser = await this.userRepository.findOne({ phone: phone });
+    if (Object.keys(findUser).length > 0) {
+      if (findUser.password === password) return findUser
+      throw new HttpException('密码错误', HttpStatus.OK);
     }
+    throw new HttpException('用户未注册', HttpStatus.OK);
   }
-  // async appUserReg(userDto: UserDto): Promise<any> {
-  //   const { phone } = userDto;
-  //   let result: Number = await this.userRepository.count({ phone: phone });
-  //   if (result) throw new HttpException('用户已存在', HttpStatus.OK);
-  //   return await this.userRepository.save(userDto);
-  // }
-  // async appUserLogin(userDto: UserDto): Promise<any> {
-  //   const { phone, password } = userDto;
-  //   let findUser = await this.userRepository.findOne({ phone: phone });
-  //   if (Object.keys(findUser).length > 0) {
-  //     if (findUser.password === password) return findUser
-  //     throw new HttpException('密码错误', HttpStatus.OK);
-  //   }
-  //   throw new HttpException('用户未注册', HttpStatus.OK);
-  // }
 }
