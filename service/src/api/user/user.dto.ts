f48df2a0631d66d036e65703e7a8e0df91f58c07
Vincentyun <vincentzyc@qq.com>
2020-03-02 17:03:41 +0800

    UserDto

--- service/src/api/user/user.dto.ts
+++ service/src/api/user/user.dto.ts
@@ -1,15 +1,19 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UserDto {
+  // @IsString({ message: '验证码必须为字符类型' })
+  // @IsNotEmpty({ message: '验证码不能为空' })
+  // readonly code: string;
+
   // @IsString({ message: '姓名必须为字符类型' })
   // @IsNotEmpty({ message: '姓名不能为空' })
   // readonly name: string;
 
   @IsString({ message: '手机号必须为字符类型' })
   @IsNotEmpty({ message: '手机号不能为空' })
-  phone: string;
+  readonly phone: string;
 
   @IsString({ message: '密码必须为字符串类型' })
   @IsNotEmpty({ message: '密码不能为空' })
-  password: string;
+  readonly password: string;
 }
