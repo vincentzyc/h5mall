4618b9087f8f8736d04d282f4c4ca7da7191beb6
Vincentyun <vincentzyc@qq.com>
2020-03-10 18:05:36 +0800

    user

--- service/src/service/user.ts
+++ service/src/service/user.ts
@@ -3,31 +3,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity, UserResult } from '@src/entity/user';
 import { UserDto } from '@src/dto/user.dto';
-import * as _ from 'lodash';
 
-interface UserQuery {
-  pageSize?: number;
-  pageIndex?: number;
-}
+// interface UserQuery {
+//   pageSize?: number;
+//   pageIndex?: number;
+// }
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
-  ) { }
+  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) { }
 
-  async findAll(query: UserQuery): Promise<UserResult> {
-    const [users, total] = await this.userRepository
-      .createQueryBuilder('user')
-      .offset(((query.pageIndex || 1) - 1) * (query.pageSize || 0)) // 从多少条开始
-      .limit(query.pageSize) // 查询多少条数据
-      .getManyAndCount(); // 查询到数据及个数，返回的是一个数组
-    return {
-      list: users,
-      total: total
-    };
-  }
+  // async findAll(query: UserQuery): Promise<UserResult> {
+  //   const [users, total] = await this.userRepository
+  //     .createQueryBuilder('user')
+  //     .offset(((query.pageIndex || 1) - 1) * (query.pageSize || 0)) // 从多少条开始
+  //     .limit(query.pageSize) // 查询多少条数据
+  //     .getManyAndCount(); // 查询到数据及个数，返回的是一个数组
+  //   return {
+  //     list: users,
+  //     total: total
+  //   };
+  // }
   async appUserReg(userDto: UserDto): Promise<any> {
     const { phone } = userDto;
     let result: Number = await this.userRepository.count({ phone: phone });
