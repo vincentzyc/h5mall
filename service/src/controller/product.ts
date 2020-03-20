f21531f623036db2ae7ba9207cd2d3414bf439f7
Vincentyun <vincentzyc@qq.com>
2020-03-20 09:16:39 +0800

    sysRecommend

--- service/src/controller/product.ts
+++ service/src/controller/product.ts
@@ -15,19 +15,14 @@ export class ProductController {
   getProductType() {
     return this.ProductService.getProductType();
   }
+  @Post('/sysRecommend')
+  @HttpCode(HttpStatus.OK)
+  sysRecommend() {
+    return this.ProductService.sysRecommend();
+  }
   @Post('/carouselList')
   @HttpCode(HttpStatus.OK)
   carouselList() {
-    return {
-        top:[{
-          farmer_id:'1',//	String	是	农户id	
-          img:'https://m.360buyimg.com/babel/jfs/t1/86912/31/14823/96913/5e69dfcbE356b5d01/e2a207b99f8cb3ba.png',//	String	是	轮播图片地址	
-          link_url:'123',//	String	是	商品跳转链接，456（商品id）、http://baidu.com、优惠券领取页面	
-          link_type:'1',//	String	是	跳转类型,1 商品，2 页面链接（H5、小程序），3 app（应用程序）	
-          name:'111',//	String	是	名称	
-          sort:'2',//	String	是	顺序等级	
-          state:'0',//	String	是	位置，0：头部，1：中部
-        }],
-    };
+    return this.ProductService.sysRecommend();
   }
 }
--- service/src/service/product.ts
+++ service/src/service/product.ts
@@ -35,4 +35,24 @@ export class ProductService {
       .getMany();
     return productTypes
   }
+  async sysRecommend(): Promise<any> {
+    const [products] = await this.productRepository
+      .createQueryBuilder()
+      .limit(10) // 查询多少条数据;
+      .getManyAndCount();
+    return products
+  }
+  carouselList(): Object {
+    return {
+      top: [{
+        farmer_id: '1',//	String	是	农户id	
+        img: 'https://m.360buyimg.com/babel/jfs/t1/86912/31/14823/96913/5e69dfcbE356b5d01/e2a207b99f8cb3ba.png',//	String	是	轮播图片地址	
+        link_url: '123',//	String	是	商品跳转链接，456（商品id）、http://baidu.com、优惠券领取页面	
+        link_type: '1',//	String	是	跳转类型,1 商品，2 页面链接（H5、小程序），3 app（应用程序）	
+        name: '111',//	String	是	名称	
+        sort: '2',//	String	是	顺序等级	
+        state: '0',//	String	是	位置，0：头部，1：中部
+      }],
+    };
+  }
 }
