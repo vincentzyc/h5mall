db400962e0350ddb307c140ee159c0ee24a70c3b
Vincentyun <vincentzyc@qq.com>
2020-03-20 10:36:06 +0800

    appMessage

--- service/src/module/index.ts
+++ service/src/module/index.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from '@src/service/user';
 import { ProductService } from '@src/service/product';
 import { AppWxpayService } from '@src/service/appWxpay';
+import { AppMessageService } from '@src/service/message';
 
 import { UserController } from '@src/controller/user';
 import { ProductController } from '@src/controller/product';
 import { AppWxpayController } from '@src/controller/appWxpay';
+import { AppMessageController } from '@src/controller/message';
 
 import { UserEntity } from '@src/entity/user';
 import { ProductEntity } from '@src/entity/product';
@@ -24,12 +26,14 @@ import { ProductTypeEntity } from '@src/entity/product_type';
   providers: [
     UserService,
     ProductService,
-    AppWxpayService
+    AppWxpayService,
+    AppMessageService
   ],
   controllers: [
     ProductController,
     UserController,
-    AppWxpayController
+    AppWxpayController,
+    AppMessageController
   ]
 })
 export class ApiModule { }
