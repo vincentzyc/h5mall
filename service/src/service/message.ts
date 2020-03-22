1877df5098d19c0514250ef68e06e9177ab72948
Vincentyun <vincentzyc@qq.com>
2020-03-22 15:05:42 +0800

    systemMessage

--- service/src/service/message.ts
+++ service/src/service/message.ts
@@ -15,13 +15,13 @@ export class AppMessageService {
     return [{
       user_id: body.user_id,
       create_time: Date.now(),//	long	是	系统消息创建时间	
-      from_id: 123,//	int	是	来源用户id	
-      message: '测试测试测试测试消息',//	string	是	消息内容	
-      message_head: '这是标题',//	string	是	消息标题	
-      order_item_id: 12,//	int	是	订单详情id	
-      quote_id: 11,//	itn	是	报价id	
-      state: 0,//	int	是	消息状态，0未读，1已读	
-      type: 1,//	int	是	消息类型，0系统消息，1交易消息，2报价消息，3农户通知，4普通聊天消息	
+      from_id: 123, //	int	是	来源用户id	
+      message: '这是一条测试测试测试测试消息', //	string	是	消息内容	
+      message_head: '这是标题', //	string	是	消息标题	
+      order_item_id: 12, //	int	是	订单详情id	
+      quote_id: 11, //	itn	是	报价id	
+      state: 0, //	int	是	消息状态，0未读，1已读	
+      type: 1, //	int	是	消息类型，0系统消息，1交易消息，2报价消息，3农户通知，4普通聊天消息	
     }];
   }
 }
